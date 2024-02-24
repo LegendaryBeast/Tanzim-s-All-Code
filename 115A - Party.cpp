bd8c61898d3829d348c7aa44a442086f05169c4d
Tanzim Hasan Prappo <148754938+LegendaryBeast@users.noreply.github.com>
2024-02-24 21:55:12 +0600

    Create 115A - Party.cpp

---
+++ 115A - Party.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int                 long long
+#define ll                  long long
+#define endl                '\n'
+#define ff                  first
+#define ss                  second
+#define pb                  push_back
+#define all(x)              x.begin(), x.end()
+#define sz(x)               (int)(x).size()
+
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+
+#define PI                  3.141592653589793238
+#define INF                 LONG_LONG_MAX
+#define MOD                 1e9+7
+#define in(a,n)             for(int i = 0; i < n; i++)cin >> a[i]
+#define f(i,n)              for(int i = 0; i < n; i++)
+#define FAST_IO             (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+
+ll fact(ll n)          { if(n==0) return 1; ll res = 1; for (ll i = 2; i <= n; i++) res = res * i; return res; }
+ll nPr(ll n, ll r)     { return fact(n) / fact(n - r); }
+ll nCr(ll n, ll r)     { return fact(n) / (fact(r) * fact(n - r)); }
+ll gcd(ll a, ll b)     { if (b == 0) return a; return gcd(b, a % b); }
+ll lcm(ll a, ll b)     { return (a * b) / gcd(a, b);}
+ll mypow(ll a, ll b)   { ll ans = 1; while(b){ if (b&1) ans = (ans*a) ; b /= 2; a = (a*a); } return ans; }
+bool isPrime(ll n)     { if(n <= 1) return false; for(ll i = 2; i <= sqrt(n); i++) if(n % i == 0) return false; return true; }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void solve()
+{
+    int ans=0,n,c=0;
+    cin>>n;
+    int a[n+1];
+    
+    for(int i=1; i<=n; i++)cin >> a[i];
+    
+    for(int i=1; i<=n; i++)
+    {
+        c = 0;
+        int x = a[i];
+        while(x != -1)
+        {
+            x = a[x];
+            c++;
+        }
+        ans = max(ans, c);
+    }
+    cout << ans+1 << endl;
+}
+
+int32_t main()
+{
+    FAST_IO;
+    int TC = 1;
+    //cin >> TC;
+    while (TC--) solve();
+}
+
+
